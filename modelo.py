7de0248f885b1b174a0efbdffd18a46a7cb53b17
jrxschzz <canalautomatizado2jorge@gmail.com>
2026-06-23 14:18:24 +0200

    sada

--- modelo.py
+++ modelo.py
@@ -10,3 +10,14 @@ class Incidencia:
         self.afectados = afectados
         self.riesgo = "MEDIO"  # Valor por defecto del riesgo
         
+class IncidenciaPhishing(Incidencia):
+    def __init__(self, id, titulo, descripcion, fecha, afectados, url_sospechosa):
+        super().__init__(id, titulo, descripcion, fecha, afectados)
+        self.url_sospechosa = url_sospechosa
+        self.riesgo = "ALTO"  # Valor por defecto del riesgo para phishing
+
+class IncidenciaMalware(Incidencia):
+    def __init__(self, id, titulo, descripcion, fecha, afectados, tipo_malware):
+        super().__init__(id, titulo, descripcion, fecha, afectados)
+        self.tipo_malware = tipo_malware
+        self.riesgo = "ALTO"  # Valor por defecto del riesgo para malware
